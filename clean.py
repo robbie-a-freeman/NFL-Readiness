fc9234e2dc918a6a21772a4810ee9ae3cdb54dce
Robert Freeman <zarohian@yahoo.com>
2019-06-11 23:12:01 -0500

    Initial script, which does nothing yet

---
+++ clean.py
@@ -0,0 +1,10 @@
+# @author: Robert Freeman
+
+import numpy as np
+import pandas as pd
+
+# Input data files are available in the "../input/" directory.
+# For example, running this (by clicking run or pressing Shift+Enter) will list the files in the input directory
+
+import os
+print(os.listdir("../input"))
